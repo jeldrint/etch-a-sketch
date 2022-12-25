58253f7a1b191684bd0238f73ed54a4ccb4fc876
John Eldrin Tolentino <115896724+jeldrint@users.noreply.github.com>
2022-12-26 05:09:51 +0800

    still nothing for now

--- jscript2.js
+++ jscript2.js
@@ -26,21 +26,29 @@ let sliderVal = 0;
 //Number of Grids
 document.getElementById('gridRange').oninput = function creatingGrids() {
     let numberOfGrid = document.getElementById('gridRange').value;
+    let dimension = (520-numberOfGrid)/ numberOfGrid;
 
-    let dimension = (500-numberOfGrid) / numberOfGrid;
+    sketchPad.style.width = `${520 + numberOfGrid * 2}px`;
+    sketchPad.style.height = `${520 + numberOfGrid * 2}px`;
 
     for (let i =0; i < numberOfGrid; i++){
         for (let j =0; j< numberOfGrid; j++){
             const gridBox =  document.createElement('div');
-            gridBox.classList.toggle('gridBox');
+            gridBox.classList.toggle('grid-box');
 
             gridBox.style.backgroundColor = "aliceblue";
-            gridBox.style.maxWidth = `${dimension}px`;
-            gridBox.style.maxHeight = `${dimension}px`;
-            gridBox.style.border = "solid 0.05px rgb(0,0,0,0.1)";
+            gridBox.style.width = `${dimension}px`;
+            gridBox.style.height = `${dimension}px`;
+            gridBox.style.border = "solid 1px rgb(0,0,0,0.1)";
+
+            //gridBox.style.display = "grid";
+            //gridBox.style.grid = `${dimension} / auto auto auto`;
             sketchPad.appendChild(gridBox);
         }
     }
     console.log(numberOfGrid);
+    console.log(dimension);
+    console.log(sketchPad.style.width);
+    console.log(sketchPad.style.height);
 //    sketchPad.remove(chil)
 };
