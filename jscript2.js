f46639275c34ca3850f6c918ffa978f84785258f
John Eldrin Tolentino <115896724+jeldrint@users.noreply.github.com>
2022-12-27 00:30:52 +0800

    grid creation done. now onto e.targetting. 30%

--- jscript2.js
+++ jscript2.js
@@ -3,52 +3,41 @@ const sketchPad = document.querySelector('.sketchPad');
 
 let sliderVal = 0;
 
-/* function creatingGrids(){
+
+//HERE IS THE SECTION OF GRID CREATION
+document.getElementById('gridRange').oninput = function () {
+    resetGrid();
+    gridCreation();
+};
+
+function gridCreation(){
     let numberOfGrid = document.getElementById('gridRange').value;
 
-    let dimension = (500-numberOfGrid) / numberOfGrid;
+    let dimension = (700-(numberOfGrid*2)) / numberOfGrid; //the minus part is for the border size
 
     for (let i =0; i < numberOfGrid; i++){
         for (let j =0; j< numberOfGrid; j++){
             const gridBox =  document.createElement('div');
             gridBox.classList.toggle('gridBox');
-
-            gridBox.style.backgroundColor = "aliceblue";
-            gridBox.style.width = `${dimension}px`;
-            gridBox.style.height = `${dimension}px`;
-            gridBox.style.border = "solid 0.05px rgb(0,0,0,0.1)";
-            sketchPad.appendChild(gridBox);
-        }
-    }
-    console.log(numberOfGrid);
-} */
-
-//Number of Grids
-document.getElementById('gridRange').oninput = function creatingGrids() {
-    let numberOfGrid = document.getElementById('gridRange').value;
-    let dimension = (520-numberOfGrid)/ numberOfGrid;
-
-    sketchPad.style.width = `${520 + numberOfGrid * 2}px`;
-    sketchPad.style.height = `${520 + numberOfGrid * 2}px`;
-
-    for (let i =0; i < numberOfGrid; i++){
-        for (let j =0; j< numberOfGrid; j++){
-            const gridBox =  document.createElement('div');
-            gridBox.classList.toggle('grid-box');
-
             gridBox.style.backgroundColor = "aliceblue";
             gridBox.style.width = `${dimension}px`;
             gridBox.style.height = `${dimension}px`;
             gridBox.style.border = "solid 1px rgb(0,0,0,0.1)";
 
-            //gridBox.style.display = "grid";
-            //gridBox.style.grid = `${dimension} / auto auto auto`;
             sketchPad.appendChild(gridBox);
         }
     }
     console.log(numberOfGrid);
-    console.log(dimension);
-    console.log(sketchPad.style.width);
-    console.log(sketchPad.style.height);
-//    sketchPad.remove(chil)
-};
+}
+
+function resetGrid() {
+    while (sketchPad.hasChildNodes()){
+        sketchPad.removeChild(sketchPad.firstChild);
+    }
+}
+
+//HERE IS THE SECTION OF GRID COLORING
+const buttonPressed = (e) => {
+    console.log(e.target);
+}
+sketchPad.addEventListener("click", buttonPressed);
